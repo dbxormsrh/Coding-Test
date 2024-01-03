45ab727ac643ea72c8ad0d12e051b45910f4099a
dbxormsrh <75031057+dbxormsrh@users.noreply.github.com>
2024-01-03 13:35:45 +0900

    Coding_Test
    20240103_Coding_Test

--- Programmers_python/heap/disk_controller.py
+++ Programmers_python/heap/disk_controller.py
@@ -1,30 +1,34 @@
+import heapq as hp
+
 def solution(jobs):
-    answer = 0
-    l = len(jobs)
+    time = 0
+    total_time = 0
+    len_jobs = len(jobs)
 
-    while True:
-        min = 0
-        sub_list = []
-        for i in range(len(jobs)):
-            if jobs[min][0] > jobs[i][0]:
-                min = i
+    heap = []
 
-        for i in range(len(jobs)):
-            if jobs[i][0] <= jobs[min_idx][1] + answer:
-                sub_list.append[i]
+    while (True):
+        temp_jobs = jobs.copy()
+        if not jobs and not heap:
+            break
 
-        min_pro = jobs[sub_list[0]][1]
-        min_pro_idx = 0
+        for i in temp_jobs: 
+            if i[0] <= time:
+                hp.heappush(heap, [i[1], i[0]])
+                jobs.remove(i)
+            
+        if not heap:
+            time += 1
+            continue
+        else:
+            work = hp.heappop(heap)
+            time += work[0]
+            total_time = total_time + time - work[1]
 
-        for i in sub_list:
-            if min_pro > jobs[i][1]:
-                min_pro = jobs[i][1]
-                min_pro_idx = i
-        
+    answer = total_time // len_jobs
 
     return answer
 
-
-
 if __name__ == "__main__":
     print(solution([[0, 3], [1, 9], [2, 6]]))
+    print(solution([[7, 8], [3, 5], [9, 6]]))   #jobs의 배열이 job의 요청 시간 순으로 정렬이 되어있지 않음을 고려하지 않음.
